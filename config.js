4559d39dcd65844e9d639ee45818b25d83e2d4ca
Nothingelser <129436420+Nothingelser@users.noreply.github.com>
2023-10-20 16:17:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,14 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
-  [''],
-  [''] 
+  ['19177208293', 'JAYSON-LACOSTE', true],
+  ['254102354539', 'JAYSON-LACOSTE', true],
+  ['254742809017', 'JAYSON-LACOSTE', true],
+  ['254757415472', 'JAYSON-LACOSTE', true],
 ] //Number of owners
 
-global.mods = ['919074692450','919074692450'] 
-global.prems = ['919074692450', '919074692450', '919074692450']
+global.mods = ['19177208293','19177208293'] 
+global.prems = ['19177208293', '19177208293', '19177208293']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,15 +48,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
+global.botname = 'JAYSON-LACOSTE WHATSBOT'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
-global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
-global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
-global.fgyt = 'https://youtube.com/@comedymelodych8468'
-global.fgpyp = 'https://youtube.com/@comedymelodych8468'
+global.packname = 'JAYSON🎯LACOSTE-WHATSBOT☘️' 
+global.author = 'JAYSON LACOSTE' 
+global.dygp = 'https://chat.whatsapp.com/E2MGnzNlCU8ECxUAgO5j85'
 global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
 
 //imagen
